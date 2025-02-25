21769d6ac44ca96bb0cd9a06cd5a547671a04f92
Kevin Peng <kevinpeng345@gmail.com>
2025-02-25 11:17:23 -0800

    add intake command

---
+++ src/main/java/frc/robot/commands/EndEffector/DropCoral.java
@@ -0,0 +1,25 @@
+package frc.robot.commands.EndEffector;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.EndEffector;
+
+public class DropCoral extends Command{
+    private final EndEffector m_EndEffector;
+    public DropCoral(EndEffector m_EndEffector){
+        this.m_EndEffector = m_EndEffector;
+        addRequirements(m_EndEffector);
+    }
+    @Override
+    public void initialize(){
+        m_EndEffector.setEjectSpeed();
+    }
+    @Override
+    public boolean isFinished(){
+        Debouncer m_debouncer = new Debouncer(0.2, Debouncer.DebounceType.kBoth);
+        if (m_debouncer.calculate(m_EndEffector.getBeamBreakValue() == false)){
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/frc/robot/commands/EndEffector/IntakeCoral.java
+++ src/main/java/frc/robot/commands/EndEffector/IntakeCoral.java
@@ -1,25 +1,30 @@
-package frc.robot.commands;
+package frc.robot.commands.EndEffector;
 
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.EndEffector;
 
-public class DropCoral extends Command{
+public class IntakeCoral extends Command{
     private final EndEffector m_EndEffector;
-    public DropCoral(EndEffector m_EndEffector){
+
+    public IntakeCoral(EndEffector m_EndEffector){
         this.m_EndEffector = m_EndEffector;
         addRequirements(m_EndEffector);
     }
     @Override
     public void initialize(){
-        m_EndEffector.setEjectSpeed();
+        m_EndEffector.setIntakeSpeed();
     }
     @Override
     public boolean isFinished(){
-        Debouncer m_debouncer = new Debouncer(0.2, Debouncer.DebounceType.kBoth);
-        if (m_debouncer.calculate(m_EndEffector.getBeamBreakValue() == false)){
+        Debouncer m_debouncer = new Debouncer(0.1, Debouncer.DebounceType.kBoth);
+        if (m_debouncer.calculate(m_EndEffector.getBeamBreakValue())){
             return true;
         }
         return false;
+    }
+    @Override
+    public void end(boolean interrupted){
+        m_EndEffector.setHoldSpeed();
     }
 }
